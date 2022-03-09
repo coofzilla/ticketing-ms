9869b1842f87b8742167916d7cbd18b5409c4552
coofzilla <jeric614@yahoo.com>
2022-03-09 20:01:20 +0900

    error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,7 @@ import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
 
 const app = express();
 app.use(express.json());
@@ -12,6 +13,8 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
+app.use(errorHandler);
+
 app.listen(3000, () => {
-  console.log("LISTENING ON PORT 3000");
+  console.log("LISTENING ON PORT 3000!");
 });
