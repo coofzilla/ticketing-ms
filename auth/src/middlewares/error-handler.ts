e0eab8477c656e4f17a0954b1b01cbd8a8ee9987
coofzilla <jeric614@yahoo.com>
2022-03-10 13:15:33 +0900

    subclasses f/validation errors

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -9,8 +9,7 @@ export const errorHandler = (
   console.log("Something went wrong", err);
 
   res.status(400).send({
-    message: "Something went wrong",
+    //string from throw new Error assigned to message
+    message: err.message,
   });
 };
-
-
