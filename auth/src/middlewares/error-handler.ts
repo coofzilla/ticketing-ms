0b7f8a063d33582fa459419eb6210c61405054b7
coofzilla <jeric614@yahoo.com>
2022-03-10 13:19:32 +0900

    determined error type w/instance of errors

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from "express";
+import { RequestValidationError } from "../errors/request-validation-errors";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 export const errorHandler = (
   err: Error,
@@ -6,7 +8,12 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  console.log("Something went wrong", err);
+  if (err instanceof RequestValidationError) {
+    console.log("handling error as request validation");
+  }
+  if (err instanceof DatabaseConnectionError) {
+    console.log("handling error as db connection error");
+  }
 
   res.status(400).send({
     //string from throw new Error assigned to message
